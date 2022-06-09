7a8b8d835e9650d8539daa5ce5931339a7353437
Carlo Occhiena <robotfood@virgilio.it>
2022-06-09 14:02:26 +0200

    wip

--- erp/erp_core/models.py
+++ erp/erp_core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    """Create company profile and their attributes"""
+    name = models.CharField(max_length=100)
+    vat_id = models.CharField(max_length=15, unique=True, blank=True, null=True)
+
+
+
+
+
+
+"""
+azienda (nome, partita iva, settore, città, indirizzo, cap, provincia, nazione, sito web, note)
+fattura (emittente, destinatario, data, numero, importo, tipologia di fattura, scadenza, stato pagamento, note)
+"""
