6a7b3c2437fb3d8990f1e573d9387dadebb8abb1
Carlo Occhiena <robotfood@virgilio.it>
2022-06-10 19:39:58 +0200

    update urls

--- erp/erp/urls.py
+++ erp/erp/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('erp_core.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# custom admin page
+admin.site.site_header = "ERP Admin Page"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Welcome to ERP Admin Portal"
