d37884ce21f6d77caa9feac0a05eecab4fdf33a1
Carlo Occhiena <robotfood@virgilio.it>
2022-06-21 12:47:54 +0200

    added filters expansions

--- erp/erp_core/filters.py
+++ erp/erp_core/filters.py
@@ -17,3 +17,20 @@ class CompanyFilter(django_filters.FilterSet):
             'website': ['icontains'], 
             'notes': ['icontains'], 
         }
+        
+class InvoiceFilter(django_filters.FilterSet):
+    """Allow search over the invoice DB"""
+    class Meta:
+        model = models.Invoice
+        fields = {
+            #'sender': ['icontains', 'Company__name'],  # how to use icontains with foreign key https://django-filter.readthedocs.io/en/stable/guide/usage.html?#generating-filters-with-meta-fields
+            #'receiver': ['icontains'],
+            'date': ['icontains'], 
+            'number': ['icontains'], 
+            'amount': ['icontains'], 
+            'kind': ['icontains'], 
+            'due_date': ['icontains'], 
+            'status': ['icontains'], 
+            'notes': ['icontains'], 
+        }
+        
