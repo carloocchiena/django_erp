e16960937acd029e36ae8be096ed4203f811ba7b
Carlo Occhiena <robotfood@virgilio.it>
2022-06-30 14:02:15 +0200

    filters func

--- erp/erp_core/admin.py
+++ erp/erp_core/admin.py
@@ -4,3 +4,4 @@ from . import models
 
 admin.site.register(models.Company)
 admin.site.register(models.Invoice)
+admin.site.register(models.Payment)
--- erp/erp_core/functions.py
+++ erp/erp_core/functions.py
@@ -2,13 +2,35 @@ from . import models
 
 def sum_active_invoices():
     """Sum all the invoices that the company issued to all its ctps"""
-    active_invoices = []
-    query = models.Invoice.objects.all().filter(kind='ACTIVE')
+    kind = 'ACTIVE'
+    payment_dictionary = {}
+    invoice_query = models.Invoice.objects.all().filter(kind=kind)
+    payment_query = models.Payment.objects.all().filter(kind=kind)
+    company_query = models.Company.objects.all()
     
-    for company in query:
-        active_invoices.append(company)
+    company_pks = invoice_query.values_list('receiver', flat=True)
     
-    return active_invoices
+    temp_list = list(company_query.filter(pk__in=company_pks))
+
+    print(f"company_pk:{(list(company_pks))}")
+    print(f"company_list:{(list(company_query.filter(pk__in=company_pks)))}")
+    print(f"single_company:{(temp_list[0])}")
+    print(f"queryset:{(invoice_query.values_list('receiver', flat=True))}")
+    
+    
+    #arrivato qui, tutto da costruire ma siamo a metà strada dai
+    
+    """
+    for company in temp_list:
+        value = payment_query.values_list(company)    
+        payment_dictionary[company] = value
+    
+    print(payment_dictionary)
+    """
+    
+    
+    return list("PIPPO")
+      
         
 if __name__ == '__main__':
     sum_active_invoices()
